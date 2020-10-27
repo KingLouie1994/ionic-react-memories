34db558c1853a83cc6fc19cfce2adb4c59a44a4b
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-10-27 14:02:40 +0100

    installed necessary dependency to use cameras if application is used as web app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
+
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 import MemoriesContextProvider from "./data/MemoriesContextProvider";
 
@@ -12,7 +13,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+defineCustomElements(window);
